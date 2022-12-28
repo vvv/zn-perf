a7eb4adb0d0cb7435e872e05478a21037422e9ca
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2022-12-28 23:28:06 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ use parquet::file::reader::{FileReader, SerializedFileReader};
 use std::fs::File;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let f = File::open(concat!(
+    let file = File::open(concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/dat/7013506939548213248.parquet"
     ))?;
-    let rd = SerializedFileReader::new(f)?;
-    assert_eq!(rd.num_row_groups(), 1);
+    let file = SerializedFileReader::new(file)?;
+    assert_eq!(file.num_row_groups(), 1);
 
-    let row_group_reader = rd.get_row_group(0)?;
-    assert_eq!(row_group_reader.num_columns(), 41);
+    let row_group = file.get_row_group(0)?;
+    assert_eq!(row_group.num_columns(), 41);
+    // dbg!(row_group.metadata());
     Ok(())
 }
