18cfe8cb22112ede4c44df87fab921b7f977fc70
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2022-12-28 21:32:18 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ))?;
     let rd = SerializedFileReader::new(f)?;
     assert_eq!(rd.num_row_groups(), 1);
+
+    let row_group_reader = rd.get_row_group(0)?;
+    assert_eq!(row_group_reader.num_columns(), 41);
     Ok(())
 }
