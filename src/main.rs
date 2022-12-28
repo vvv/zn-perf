63fcdf5e4f9fe282c903127d966398b087040705
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2022-12-28 15:31:14 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use parquet::file::reader::{FileReader, SerializedFileReader};
+use std::fs::File;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let f = File::open(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/dat/7013506939548213248.parquet"
+    ))?;
+    let rd = SerializedFileReader::new(f)?;
+    assert_eq!(rd.num_row_groups(), 1);
+    Ok(())
 }
