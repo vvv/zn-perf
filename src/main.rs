a4a56d9d8f8318c67f3d26f285f8fc96ca360e3f
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-01-01 19:40:15 +0200

    Show number of rows and names of columns

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use clap::Parser;
-// use parquet::file::reader::{FileReader, SerializedFileReader};
-// use std::fs::File;
-use std::path::PathBuf;
+use parquet::file::reader::{FileReader, SerializedFileReader};
+use std::{fs::File, path::PathBuf};
 
 #[derive(Debug, Parser)]
 #[command(about)]
@@ -12,6 +11,20 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
+    for path in cli.files {
+        let file = File::open(&path)?;
+        let file = SerializedFileReader::new(file)?;
+        let file_metadata = file.metadata().file_metadata();
+        println!(
+            "{} has {} rows in {} row group(s)",
+            path.display(),
+            file_metadata.num_rows(),
+            file.num_row_groups()
+        );
+        for (i, col) in file_metadata.schema().get_fields().iter().enumerate() {
+            println!("    column {i}: {}", col.name());
+        }
+    }
     /* XXX
     let file = File::open(concat!(
         env!("CARGO_MANIFEST_DIR"),
