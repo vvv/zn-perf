c17dc0b845d5584d17bd6ba0beeccc45ee4f177b
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-01-01 15:34:00 +0200

    Apply `clap`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use parquet::file::reader::{FileReader, SerializedFileReader};
-use std::fs::File;
+use clap::Parser;
+// use parquet::file::reader::{FileReader, SerializedFileReader};
+// use std::fs::File;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[command(about)]
+struct Cli {
+    /// Input files
+    files: Vec<PathBuf>,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dbg!(zn_perf::fibonacci(5));
+    let cli = Cli::parse();
+    /* XXX
     let file = File::open(concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/dat/7013506939548213248.parquet"
@@ -13,5 +23,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let row_group = file.get_row_group(0)?;
     assert_eq!(row_group.num_columns(), 41);
     // dbg!(row_group.metadata());
+    // XXX */
     Ok(())
 }
