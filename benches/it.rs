fb1497830283d09d45d0038df833e3da918d3632
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-01-02 15:40:12 +0200

    bench: Increase measurement time from 5s to 30s
    This addresses the following warning of `cargo bench`:
    
    > Warning: Unable to complete 100 samples in 5.0s. You may wish to increase target time to 22.7s, or reduce sample count to 20.

--- benches/it.rs
+++ benches/it.rs
@@ -1,4 +1,5 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use std::time::Duration;
 
 fn bench(c: &mut Criterion) {
     let parquet_file = zn_perf::new_mem_reader(concat!(
@@ -7,9 +8,13 @@ fn bench(c: &mut Criterion) {
     ))
     .unwrap();
 
-    c.bench_function("read-all", |b| {
+    let mut group = c.benchmark_group("parquet-io");
+    group.measurement_time(Duration::from_secs(30));
+
+    group.bench_function("read-all", |b| {
         b.iter(|| zn_perf::read_all_data(&parquet_file).unwrap())
     });
+    group.finish();
 }
 
 criterion_group!(benches, bench);
