2bc761ed46fa0bceffc7c53b407e964ff6e8b912
Valeriy V. Vorotyntsev <valery.vv@gmail.com>
2023-01-02 15:33:41 +0200

    Benchmark parquet I/O

--- benches/it.rs
+++ benches/it.rs
@@ -1,8 +1,15 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 fn bench(c: &mut Criterion) {
-    todo!()
-    //XXX c.bench_function("fib 20", |b| b.iter(|| zn_perf::fibonacci(black_box(20))));
+    let parquet_file = zn_perf::new_mem_reader(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/dat/7013506939548213248.parquet"
+    ))
+    .unwrap();
+
+    c.bench_function("read-all", |b| {
+        b.iter(|| zn_perf::read_all_data(&parquet_file).unwrap())
+    });
 }
 
 criterion_group!(benches, bench);
